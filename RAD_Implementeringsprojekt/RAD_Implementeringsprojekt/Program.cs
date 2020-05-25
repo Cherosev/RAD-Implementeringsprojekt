9139a9f5b15ae53a4e875ed53028fd3942056c7d
Søren Brix <SRB@pfa.dk>
2020-05-25 13:50:37 +0200

    Oprettet projekt.

---
+++ RAD_Implementeringsprojekt/RAD_Implementeringsprojekt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RAD_Implementeringsprojekt
+{
+    // Strøm at par (x1, d1)...(xn dn) hvor xn er en nøgle i 64-bit, og dn er et heltal (både positivt og negativt).
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
